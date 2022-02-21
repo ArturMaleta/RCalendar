f081d7a73a49d80dac2feb7b078e285ebf836f71
Artur_Maleta <artur.maleta@vmlyr.com>
2022-02-21 15:54:12 +0100

    Initial Commit

---
+++ SwiftUISamples/SwiftUISamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISamplesApp.swift
+//  SwiftUISamples
+//
+//  Created by Artur Małeta on 21/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
